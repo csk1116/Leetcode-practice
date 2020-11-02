77133096438461a986a0f6990b1635748284da4d
csk1116 <u1116eric777@g-mail.nsysu.edu.tw>
2020-11-02 17:24:06 +0800

    add a solution

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Leetcode-practice
+
+## Array
+---
+|#|Title|solutions|Time|Space|Difficulty|Note|Member|
+|:-----|:-----|:-----|:-----|:-----|:-----|:---|:---|
+|0026|Remove Duplicate from Sorted Array|
---
+++ Solutions/cpp/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+                 
+        if(nums.size()==0) return 0;
+        
+        int k = 0;
+        
+        for(int i=1; i<nums.size(); ++i){
+            
+            if(nums[k] != nums[i]){
+                k += 1;
+                nums[k] = nums[i];
+            }
+        }
+        
+        return k+1;
+        
+        
+    }
+};
