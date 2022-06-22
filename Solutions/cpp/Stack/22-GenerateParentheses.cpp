82522d83539c852759b532cac422ddc37938a848
csk1116 <u1116eric777@gmail.com>
2022-06-22 10:56:27 +0800

    revise prob 22.

--- Solutions/cpp/Stack/22-GenerateParentheses.cpp
+++ Solutions/cpp/Stack/22-GenerateParentheses.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 //BackTracking
-//Time:O(N)
+//Time:O()
 //Space:O()
 class Solution1
 {    
@@ -18,18 +18,21 @@ public:
         backTrack(result, validParentheses, open, close);            
         return result;
     } 
-    void backTrack(vector<string>& vec, string s, int open, int close)
+    void backTrack(vector<string>& vec, string str, int open, int close)
     {
+        //terminate condition
         if(open == 0 && close == 0)
         {
-            vec.push_back(s);
+            vec.push_back(str);
             return;
         }
         
+        //(open <= close) is the only situation that the parentheses is valid 
+        // ")" -> open = 3, close = 2 -> invalid, "())" -> open = 2, close = 1 -> invalid)
         if(open <= close && open >= 0 && close >= 0)
         {           
-            backTrack(vec, (s + "("), open - 1, close);
-            backTrack(vec, (s + ")"), open, close -1);
+            backTrack(vec, str + "(", open - 1, close);
+            backTrack(vec, str + ")", open, close - 1);
         }        
     }  
 };
@@ -40,7 +43,29 @@ public:
 class Solution2
 {
 public:
-
+    vector<string> generateParenthesis(int n) 
+    {     
+        vector<string> result;
+        string validParentheses = "";
+        backTrack(result, validParentheses, 0, 0, n);            
+        return result;
+    } 
+    void backTrack(vector<string>& vec, string str, int open, int close, int max)
+    {
+        //terminate condition
+        if(open == max && close == max)
+        {
+            vec.push_back(str);
+            return;
+        }
+        
+        //(open > close) is the only situation that we can add a closing parentheses ")" 
+        // ")" -> open = 0, close = 1 -> invalid, "())" -> open = 1, close = 2 -> invalid)
+        if(open < max)
+            backTrack(vec, str + "(", open + 1, close, max);
+        if(open > close)
+            backTrack(vec, str + ")", open, close + 1, max);             
+    }
 };
 
 void ShowResult(const vector<string>& vec)
@@ -64,6 +89,13 @@ int main()
     myResult = solution1.generateParenthesis(2);
     ShowResult(myResult);
     myResult = solution1.generateParenthesis(3);
+    ShowResult(myResult);
+
+    myResult = solution2.generateParenthesis(1);
+    ShowResult(myResult);
+    myResult = solution2.generateParenthesis(2);
+    ShowResult(myResult);
+    myResult = solution2.generateParenthesis(3);
     ShowResult(myResult);
     
     return 0;
